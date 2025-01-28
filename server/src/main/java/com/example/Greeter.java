49673095a8cb30c3f74904e8ad054a617b762ce7
Jorge Aguirre <jorge25x@hotmail.com>
2025-01-27 23:02:02 -0500

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,10 +12,7 @@ public class Greeter {
 
   }
   
-  /**
-  * @param el nombre de una peersona
-  * @return caracter greeting
-  */
+/** {@inheritDoc} */
   
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
